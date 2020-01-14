70d668d0c0a8795f875bea430fd4fa9d71ba038f
varlackc <varlackc@mail.gvsu.edu>
2020-01-13 23:29:48 -0600

    Update

---
+++ Exercise_4_12/Exercise_4_12/Date.cs
@@ -0,0 +1,47 @@
+﻿/*
+ 4.12 (Date Class) Create a class called Date 
+ that includes three pieces of information as 
+ auto-implemented properties—a month (type int),
+ a day (type int) and a year (type int). 
+ 
+ Your class should have a constructor that initializes 
+ the three automatic properties and assumes 
+ that the values provided are correct. 
+ 
+ Provide a method DisplayDate that displays the month, 
+ day and year separated by forward slashes (/). 
+ 
+ Write a test app named DateTest that demonstrates 
+ class Date’s capabilities.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_4_12
+{
+    public class Date
+    {
+        //Declare auto increment properties
+        public int Month { get; set; }
+        public int Day { get; set; }
+        public int Year { get; set; }
+
+        //Constructor to initialize the properties
+        public Date(int month, int day, int year)
+        {
+            Month = month;
+            Day = day;
+            Year = year;
+        }
+
+        // Display the date 
+        public void DisplayDate() {
+            Console.WriteLine("\t "+Month+" / "+Day+" / "+Year+" ");
+        }
+
+    }
+}
