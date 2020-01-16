983e8670466c861491d00e21d29b8ea113d0ff85
varlackc <varlackc@mail.gvsu.edu>
2020-01-15 23:03:19 -0600

    Update

---
+++ Exercises_10_3/Exercises_10_3/Rectangle.cs
@@ -0,0 +1,44 @@
+﻿/*
+ 10.3 (Rectangle Class) Create class Rectangle. 
+ The class has attributes length and width, each of which defaults to 1. 
+ It has read-only properties that calculate the Perimeter and the Area of the rectangle. 
+ It has properties for both length and width. 
+ 
+ The set accessors should verify that length and width are each floating-point numbers 
+ greater than 0.0 and less than 20.0. Write an app to test class Rectangle.
+ */
+using System;
+
+namespace Exercises_10_3
+{
+    public class Rectangle
+    {
+        public float Length { get; set; } =  (float) 1.0;
+        public float Width { get; set; } = (float) 1.0;
+
+        // set parameters from 1.1 to 20.0
+        public Rectangle(float length, float width)
+        {
+            if (length <= 0 || width <= 0 || length > 20 || width > 20)
+            {
+                Console.WriteLine("There is an error with the input");
+            }
+
+            else
+            {
+                Length = length;
+                Width = width;
+            }
+        }
+
+        public float Perimeter() {
+            float result = (2 * Length) + (2*Width);
+            return result;
+        }
+
+        public float Area() {
+            float result = (Length * Width);
+            return result;
+        }
+    }
+}
---
+++ Exercises_10_3/Exercises_10_3/RectangleTest.cs
@@ -0,0 +1,58 @@
+﻿/*
+ 10.3 (Rectangle Class) Create class Rectangle. 
+ The class has attributes length and width, each of which defaults to 1. 
+ It has read-only properties that calculate the Perimeter and the Area of the rectangle. 
+ It has properties for both length and width. 
+ 
+ The set accessors should verify that length and width are each floating-point numbers 
+ greater than 0.0 and less than 20.0. Write an app to test class Rectangle.
+ */
+
+using System;
+
+namespace Exercises_10_3
+{
+    class RectangleTest
+    {
+        static void Main(string[] args)
+        {
+
+            //declare variables
+            float length = (float) 5.0;
+            float width = (float) 5.0;
+
+            // Explain the program to the user
+            ExplainToUser();
+
+            // Display the parameters
+            DisplayParameters(length, width);
+
+            // Declare new invoice object
+            Rectangle sampleRectangles = new Rectangle(length, width);
+
+            // Desplay the result
+            Console.WriteLine(" Perimeter: "+sampleRectangles.Perimeter());
+            Console.WriteLine(" Area: " + sampleRectangles.Area());
+
+            Console.ReadLine();
+        }
+
+        //Explain the program to the user
+        public static void ExplainToUser()
+        {
+            Console.WriteLine();
+            Console.WriteLine("\t =======================================");
+            Console.WriteLine("\t| Rectangle Test Application \t\t|");
+            Console.WriteLine("\t =======================================");
+            Console.WriteLine();
+        }
+
+        //Show the parameters to the user
+        public static void DisplayParameters( float length, float width)
+        {
+            Console.WriteLine("\t Length: " + length);
+            Console.WriteLine("\t Width: " + width);
+            Console.WriteLine();
+        }
+    }
+}
