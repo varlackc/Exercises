22e1f28b8cb32f2b8c10332438c1607c6e40dce5
varlackc <varlackc@mail.gvsu.edu>
2020-01-28 22:28:01 -0600

    Create Cube.cs

---
+++ Exercise12_10/Exercise12_10/Cube.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercise12_10
+{
+    class Cube : ThreeDimensionalShape
+    {
+        public Cube(double a) : base(a) { }
+
+        public override double Area
+        {
+            get { return 6 * Math.Pow(base.Side,2); }
+        }
+
+        public override double Volume
+        {
+            get { return Math.Pow(base.Side,3); }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Cube") + base.ToString();
+        }
+    }
+}
