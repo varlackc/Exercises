4068e59b57e8c79e8a9f1e70bad423e6a4da5d16
varlackc <varlackc@mail.gvsu.edu>
2020-01-28 22:27:57 -0600

    Create Circle.cs

---
+++ Exercise12_10/Exercise12_10/Circle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercise12_10
+{
+    class Circle : TwoDimensionalShape
+    {
+        public Circle(double a) : base(a) { }
+
+        public override double Area
+        {
+            get { return Math.PI * Math.Pow(base.Side,2); }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Circle") + base.ToString();
+        }
+    }
+}
