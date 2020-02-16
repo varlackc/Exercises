20aaf50dfc53f3371b060ba05fa3d72105b459bd
varlackc <varlackc@mail.gvsu.edu>
2020-02-15 23:52:59 -0600

    Create Form1.cs

---
+++ Exercise_17_3/Exercise_17_3/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Exercise_17_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //define the file path and use a verbatim literal to make the code more readable
+            string filePath = @"C:\Users\Owner\source\repos\Exercise_17_3\Exercise_17_3\grades.txt";
+            if (File.Exists(filePath))
+            {
+                StreamReader stream = new StreamReader(filePath);
+                textBox1.AppendText(stream.ReadToEnd());
+            }
+            else
+            {
+                MessageBox.Show("The is an error trying to Read The Document");
+            }
+        }
+    }
+}
