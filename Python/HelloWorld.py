2e7cd3a82397ff1ee09d74e0c0ffa7d7861a4f07
Maxwell Varlack <varlackc@mail.gvsu.edu>
2023-11-03 21:21:05 -0600

    update the Hello World example in python

---
+++ Python/HelloWorld.py
@@ -0,0 +1,19 @@
+########################################################################
+#                                                                      #
+# This exercise is used to practice the Python language.               #
+#                                                                      #
+# Write a program that prints a message to the user in the terminal.   #
+# The message should say "Hello World!"                                #
+#                                                                      #
+########################################################################
+
+__author__ = "Maxwell Varlack"
+__version__ = "0.0.1"
+
+def main():
+    print("Hello World")
+    input("Press any key to exit the program: ... ")
+    pass
+
+if __name__ == "__main__":
+    main()
